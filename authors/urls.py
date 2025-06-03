8d58e8e2d1cd5cf4d6371985f8f00bf4d71aafa4
Thiago Fernandes <lalaaks22@gmail.com>
2025-06-03 16:07:14 -0300

    crinado a url 'me'

--- authors/urls.py
+++ authors/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
+from rest_framework.routers import SimpleRouter
 from . import views
 
+
 app_name = 'authors'
+
+author_api_router = SimpleRouter()
+author_api_router.register('api', views.AuthorViewSet, basename='author_api')
+
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
@@ -32,3 +37,5 @@ urlpatterns = [
         name='profile'
     ),
 ]
+
+urlpatterns += author_api_router.urls
